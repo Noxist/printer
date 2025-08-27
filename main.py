6cd4f4e80b7c744003ac51681837494595c5eba6
Leandro Ron Emanuel Aeschbacher <leandro.aeschbacher77@gmail.com>
2025-08-27 14:23:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,12 +29,9 @@ from logic import (
 )
 from ui_html import html_page, HTML_UI, settings_html_form, guest_ui_html
 
-from api_bom import router as bom_router  # BOM API als separater router einbinden
-
 app = FastAPI(title="Printer API")
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
 
-app.include_router(bom_router)  # Register BOM APIs
 app.include_router(sources_router)
 
 PRINT_WIDTH_PX = int(cfg_get("PRINT_WIDTH_PX", 576))
